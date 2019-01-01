f3a35a74a4f980e96ac12f7ceec953134fe5b3eb
Obiwac <obiwac@gmail.com>
2019-01-01 14:09:03 +0100

    Added sub2pewds live wallpaper for OpenGL on desktops

--- src/functions/video.h
+++ src/functions/video.h
@@ -27,7 +27,10 @@
 	#if KOS_USES_JNI
 		unsigned char waiting_video_flip = 0;
 	#endif
-
+	
+	void __update_predefined_texture(unsigned long long name);
+	static unsigned char predefined_textures_live = 0;
+	
 	void video_flip(void) {
 		surface_layer_offset = 0.0f;
 		
@@ -46,6 +49,12 @@
 		#if KOS_3D_VISUALIZATION
 			glRotatef(1.0f, 0.0f, 1.0f, 0.0f);
 		#endif
+		
+		if (predefined_textures_live) {
+			__update_predefined_texture(TEXTURE_BACKGROUND);
+			__update_predefined_texture(TEXTURE_FROSTED_BACKGROUND);
+			
+		}
 		
 	}
 	
@@ -216,36 +225,146 @@
 	static unsigned long long predefined_background_texture_dimensions      [2];
 	static unsigned long long predefined_frost_background_texture_dimensions[2];
 	
+	// live predefined textures
+	
+	static unsigned long long* predefined_background_texture_data;
+	static unsigned long long* predefined_frost_background_texture_data;
+	
+	static GLuint predefined_background_texture_shader;
+	static GLuint predefined_frost_background_texture_shader;
+	
+	static GLuint predefined_background_texture_framebuffer;
+	static GLuint predefined_frost_background_texture_framebuffer;
+	
+	static surface_t predefined_texture_surface_dummy;
+	
 	void bmp_load(unsigned long long ____this, unsigned long long _path);
 	void bmp_free(unsigned long long ____this);
 	
+	#define KOS_SUB2PEWDS KOS_USES_OPENGL_DESKTOP
+	
 	static int kos_setup_predefined_textures(kos_t* __this) {
 		int warning = 0;
-		bitmap_image_t temp_bmp;
 		
-		bmp_load((unsigned long long) &temp_bmp, (unsigned long long) "wallpaper/wallpaper.bmp");
-		predefined_background_texture = texture_create((unsigned long long) temp_bmp.data, temp_bmp.bpp, temp_bmp.width, temp_bmp.height);
-		
-		predefined_background_texture_dimensions[0] = temp_bmp.width;
-		predefined_background_texture_dimensions[1] = temp_bmp.height;
-		
-		bmp_free((unsigned long long) &temp_bmp);
-		warning += predefined_background_texture == -1;
-		
-		bmp_load((unsigned long long) &temp_bmp, (unsigned long long) "wallpaper/frost.bmp");
-		predefined_frost_background_texture = texture_create((unsigned long long) temp_bmp.data, temp_bmp.bpp, temp_bmp.width, temp_bmp.height);
-		
-		predefined_frost_background_texture_dimensions[0] = temp_bmp.width;
-		predefined_frost_background_texture_dimensions[1] = temp_bmp.height;
-		
-		bmp_free((unsigned long long) &temp_bmp);
-		warning += predefined_frost_background_texture == -1;
+		#if KOS_SUB2PEWDS
+			predefined_textures_live = 1;
+			
+			surface_new        ((unsigned long long) &predefined_texture_surface_dummy, -_UI64_MAX_MARGIN, -_UI64_MAX_MARGIN, _UI64_MAX_MARGIN << 1, _UI64_MAX_MARGIN << 1);
+			surface_set_texture((unsigned long long) &predefined_texture_surface_dummy, 0);
+			
+			predefined_background_texture_dimensions      [0] = video_width();
+			predefined_background_texture_dimensions      [1] = video_height();
+			
+			predefined_frost_background_texture_dimensions[0] = video_width();
+			predefined_frost_background_texture_dimensions[1] = video_height();
+			
+			int bpp = 32;
+			
+			predefined_background_texture_data       = (unsigned long long*) malloc((bpp >> 3) * predefined_background_texture_dimensions      [0] * predefined_background_texture_dimensions      [1]);
+			predefined_frost_background_texture_data = (unsigned long long*) malloc((bpp >> 3) * predefined_frost_background_texture_dimensions[0] * predefined_frost_background_texture_dimensions[1]);
+			
+			predefined_background_texture       = __texture_create(predefined_background_texture_data,       bpp, predefined_background_texture_dimensions      [0], predefined_background_texture_dimensions      [1], 0);
+			predefined_frost_background_texture = __texture_create(predefined_frost_background_texture_data, bpp, predefined_frost_background_texture_dimensions[0], predefined_frost_background_texture_dimensions[1], 0);
+			
+			predefined_background_texture_framebuffer       = framebuffer_create(predefined_background_texture);
+			predefined_frost_background_texture_framebuffer = framebuffer_create(predefined_frost_background_texture);
+			
+			warning += gl_load_shaders((GLuint*) &predefined_background_texture_shader,
+			R"shader-code(
+				#version 120
+				
+				void main(void) {
+					gl_Position = ftransform();
+					
+				}
+			)shader-code", R"shader-code(
+				#version 120
+				
+				uniform sampler2D sampler_texture;
+				uniform int       has_texture;
+				uniform int       time;
+				
+				void main(void) {
+					int time_ = time / 5;
+					vec2 coord = gl_FragCoord.xy + vec2(time_ / 200.0f, time_ / 600.0f);
+					
+					float x = coord.x - coord.y / 2.0f;
+					float y = coord.y + coord.x / 4.0f;
+					
+					float random = (sin((y + sin(x / 50.0f) * (100.0f + sin(time_ / 5000.0f) * 10.0f)) / 10.0f) + 1.0f) / 2.0f;
+					
+					if (random > sin(time_ / 3000.0f) / 6.0f + 0.4f) gl_FragColor = vec4(1.0f, 0.0f, 0.3f, 1.0f);
+					else                                             gl_FragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+					
+				}
+			)shader-code");
+			
+			warning += gl_load_shaders((GLuint*) &predefined_frost_background_texture_shader,
+			R"shader-code(
+				#version 120
+				
+				void main(void) {
+					gl_Position = ftransform();
+					
+				}
+			)shader-code", R"shader-code(
+				#version 120
+				
+				uniform sampler2D sampler_texture;
+				uniform int       has_texture;
+				uniform int       time;
+				
+				void main(void) {
+					int time_ = time / 5;
+					vec2 coord = gl_FragCoord.xy + vec2(time_ / 200.0f, time_ / 600.0f);
+					
+					float x = coord.x - coord.y / 2.0f;
+					float y = coord.y + coord.x / 4.0f;
+					
+					float random = (sin((y + sin(x / 50.0f) * (100.0f + sin(time_ / 5000.0f) * 10.0f)) / 10.0f) / 20.0f + 1.0f) / 2.0f;
+					gl_FragColor = vec4(random * 1.0f, random * 0.0f, random * 0.3f, 1.0f);
+					
+				}
+			)shader-code");
+		#else
+			bitmap_image_t temp_bmp;
+			
+			bmp_load((unsigned long long) &temp_bmp, (unsigned long long) "wallpaper/wallpaper.bmp");
+			predefined_background_texture = texture_create((unsigned long long) temp_bmp.data, temp_bmp.bpp, temp_bmp.width, temp_bmp.height);
+			
+			predefined_background_texture_dimensions[0] = temp_bmp.width;
+			predefined_background_texture_dimensions[1] = temp_bmp.height;
+			
+			bmp_free((unsigned long long) &temp_bmp);
+			warning += predefined_background_texture == -1;
+			
+			bmp_load((unsigned long long) &temp_bmp, (unsigned long long) "wallpaper/frost.bmp");
+			predefined_frost_background_texture = texture_create((unsigned long long) temp_bmp.data, temp_bmp.bpp, temp_bmp.width, temp_bmp.height);
+			
+			predefined_frost_background_texture_dimensions[0] = temp_bmp.width;
+			predefined_frost_background_texture_dimensions[1] = temp_bmp.height;
+			
+			bmp_free((unsigned long long) &temp_bmp);
+			warning += predefined_frost_background_texture == -1;
+		#endif
 		
 		return warning;
 		
 	}
 	
 	static void kos_free_predefined_textures(kos_t* __this) {
+		if (predefined_textures_live) {
+			gl_delete_shader_program((GLuint*) &predefined_background_texture_shader);
+			gl_delete_shader_program((GLuint*) &predefined_frost_background_texture_shader);
+			
+			framebuffer_remove(predefined_background_texture_framebuffer);
+			framebuffer_remove(predefined_frost_background_texture_framebuffer);
+			
+			free(predefined_background_texture_data);
+			free(predefined_frost_background_texture_data);
+			
+		}
+		
 		if (predefined_background_texture       != -1) texture_remove(predefined_background_texture);
 		if (predefined_frost_background_texture != -1) texture_remove(predefined_frost_background_texture);
 		
@@ -286,12 +405,40 @@
 	
 	#define GET_PREDEFINED_TEXTURE_SIZES 1
 	
-	void update_predefined_texture(unsigned long long name) {
+	void __update_predefined_texture(unsigned long long name) {
 		switch (name) {
 			case TEXTURE_BACKGROUND: {
+				if (predefined_textures_live) {
+					GLuint default_shader = 0;
+					
+					framebuffer_bind(predefined_background_texture_framebuffer, 0, 0, video_width(), video_height());
+					gl_use_shader_program((GLuint*) &predefined_background_texture_shader);
+					
+					video_clear();
+					surface_draw((unsigned long long) &predefined_texture_surface_dummy);
+					
+					framebuffer_bind(0, 0, 0, video_width(), video_height());
+					gl_use_shader_program((GLuint*) &default_shader);
+					
+				}
+				
 				break;
 				
 			} case TEXTURE_FROSTED_BACKGROUND: {
+				if (predefined_textures_live) {
+					GLuint default_shader = 0;
+					
+					framebuffer_bind(predefined_frost_background_texture_framebuffer, 0, 0, video_width(), video_height());
+					gl_use_shader_program((GLuint*) &predefined_frost_background_texture_shader);
+					
+					video_clear();
+					surface_draw((unsigned long long) &predefined_texture_surface_dummy);
+					
+					framebuffer_bind(0, 0, 0, video_width(), video_height());
+					gl_use_shader_program((GLuint*) &default_shader);
+					
+				}
+				
 				break;
 				
 			} default: {
@@ -301,6 +448,10 @@
 			}
 			
 		}
+		
+	}
+	
+	void update_predefined_texture(unsigned long long name) {
 		
 	}
 	
