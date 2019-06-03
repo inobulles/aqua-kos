cda99c0dacbec27fa2a72f0e94d1e0cdc60788db
Obiwac <obiwac@gmail.com>
2019-06-03 22:00:58 +0200

    Cleaned up requests device

--- src/devices/requests.h
+++ src/devices/requests.h
@@ -20,8 +20,8 @@
 	typedef struct {
 		unsigned long long code;
 		
-		#define                MAX_URL_LENGTH_BYTES 4096
-		unsigned long long url[MAX_URL_LENGTH_BYTES / sizeof(unsigned long long)];
+		#define  MAX_URL_LENGTH_BYTES 8192
+		char url[MAX_URL_LENGTH_BYTES];
 		
 		unsigned long long text_bytes;
 		unsigned long long text;
@@ -42,10 +42,10 @@
 		unsigned long long url_bytes = strlen(kos_current_request.url);
 		if (url_bytes >= MAX_URL_LENGTH_BYTES) {
 			printf("WARNING Request URL is too long (%lld bytes, %s)\n", url_bytes, kos_current_request.url);
-			strcpy((char*) this->url, "__URL_TOO_LONG__");
+			strcpy(this->url, "__URL_TOO_LONG__");
 			
 		} else {
-			strcpy((char*) this->url, kos_current_request.url);
+			strcpy(this->url, kos_current_request.url);
 			
 		}
 		
@@ -58,23 +58,17 @@
 	
 	void kos_requests_free(kos_request_response_t* this) {
 		if (this->text) {
-			//~ free((void*) this->text /* this->text_bytes */);
+			free((void*) this->text /* this->text_bytes */);
 			
 		}
 		
 	}
 	
-	typedef struct {
-		kos_request_response_t request_response;
-		unsigned long long     pointer_to_const_url;
-		
-	} request_device_struct_t;
-	
 	static void requests_device_handle(unsigned long long** result, const char* data) {
-		request_device_struct_t* request_device_struct = (request_device_struct_t*) *((unsigned long long*) (data + 8));
+		unsigned long long* command = (unsigned long long*) data;
 		
-		if      (strcmp(data, "get") == 0) kos_requests_get (&request_device_struct->request_response, (const char*) request_device_struct->pointer_to_const_url);
-		else if (strcmp(data, "fre") == 0) kos_requests_free(&request_device_struct->request_response);
+		if      (command[0] == 'g') kos_requests_get ((kos_request_response_t*) command[1], (const char*) command[2]);
+		else if (command[0] == 'f') kos_requests_free((kos_request_response_t*) command[1]);
 		else KOS_DEVICE_COMMAND_WARNING("requests")
 		
 	}
