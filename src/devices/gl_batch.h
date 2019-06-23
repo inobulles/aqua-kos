228e48b72cfa20387299637c1c426024be3b5f53
Obiwac <obiwac@gmail.com>
2019-06-23 20:42:26 +0200

    Added batch colour command

--- src/devices/gl_batch.h
+++ src/devices/gl_batch.h
@@ -191,92 +191,12 @@
 			memcpy(object->texture_coords + old_coords_count * sizeof(kos_gl_batch_device_batch_texture_coord_t), (void*) model->coords_pointer, model->meta.coords_count * sizeof(kos_gl_batch_device_batch_texture_coord_t));
 			memcpy(object->indices        +  old_index_count * sizeof(object->indices[0]),                        (void*) model->index_pointer,  model->meta. index_count * sizeof(object->indices[0]));
 			
-		} else if (batch_command[0] == 'a') { // add
-			unsigned long long old_vertex_count = object->vertex_count; // get the current counts of the batch
-			unsigned long long old_index_count  = object->index_count;
-			
-			if (object->vertex_count == 0) { // if nothing yet in the batch, simply malloc
-				object->vertex_count += batch_command[3];
-				object-> index_count += batch_command[9] * 3;
-				
-				object->positions      = (kos_gl_batch_device_batch_position_t*)      malloc(object->vertex_count * sizeof(kos_gl_batch_device_batch_position_t));
-				object->colours        = (kos_gl_batch_device_batch_colour_t*)        malloc(object->vertex_count * sizeof(kos_gl_batch_device_batch_colour_t));
-				object->texture_coords = (kos_gl_batch_device_batch_texture_coord_t*) malloc(object->vertex_count * sizeof(kos_gl_batch_device_batch_texture_coord_t));
-				
-				object->indices        = (uint32_t*)                                  malloc(object->index_count  * sizeof(uint32_t));
-				
-			} else { // if adding ontop of existing objects, realloc
-				object->vertex_count += batch_command[3];
-				object-> index_count += batch_command[9] * 3;
-				
-				object->positions      = (kos_gl_batch_device_batch_position_t*)      realloc(object->positions,      object->vertex_count * sizeof(kos_gl_batch_device_batch_position_t));
-				object->colours        = (kos_gl_batch_device_batch_colour_t*)        realloc(object->colours,        object->vertex_count * sizeof(kos_gl_batch_device_batch_colour_t));
-				object->texture_coords = (kos_gl_batch_device_batch_texture_coord_t*) realloc(object->texture_coords, object->vertex_count * sizeof(kos_gl_batch_device_batch_texture_coord_t));
-				
-				object->indices        = (uint32_t*)                                  realloc(object->indices,        object->index_count  * sizeof(uint32_t));
-				
-			}
-			
-			// add to batch
-			
-			kos_gl_device_vertex_line_face_t* faces = (kos_gl_device_vertex_line_face_t*) batch_command[8];
-			
-			kos_gl_device_vertex_t*        positions      = (kos_gl_device_vertex_t*)        batch_command[2];
-			kos_gl_device_colour_t*        colours        = (kos_gl_device_colour_t*)        batch_command[4];
-			kos_gl_device_texture_coord_t* texture_coords = (kos_gl_device_texture_coord_t*) batch_command[6];
-			
-			object->has_texture = (unsigned char) (batch_command[7] ? 1 : 0);
-			
-			unsigned long long i;
-			for (i = old_vertex_count; i < object->vertex_count; i++) { // loop through each vertex of added object
-				unsigned long long absolute = i - old_vertex_count;
-				
-				object->positions[i].x = (GLfloat) positions[absolute].x / FLOAT_ONE / 2;
-				object->positions[i].y = (GLfloat) positions[absolute].y / FLOAT_ONE / 2;
-				object->positions[i].z = (GLfloat) positions[absolute].z / FLOAT_ONE / 2;
-				
-				if (batch_command[5]) {
-					object->colours[i].r = (GLfloat) colours[absolute].r / FLOAT_ONE;
-					object->colours[i].g = (GLfloat) colours[absolute].g / FLOAT_ONE;
-					object->colours[i].b = (GLfloat) colours[absolute].b / FLOAT_ONE;
-					object->colours[i].a = (GLfloat) colours[absolute].a / FLOAT_ONE;
-					
-				} else {
-					object->colours[i].r = 1.0f;
-					object->colours[i].g = 1.0f;
-					object->colours[i].b = 1.0f;
-					object->colours[i].a = 1.0f;
-					
-				}
-				
-				object->texture_coords[i].x = 0.0f;
-				object->texture_coords[i].y = 0.0f;
-				
-			} for (i = old_index_count / 3; i < object->index_count / 3; i++) {
-				unsigned long long absolute = i - old_index_count;
-				
-				if (object->has_texture) {
-					for (unsigned long long k = 0; k < 3; k++) {
-						unsigned long long index = absolute;
-						
-						unsigned long long j;
-						for (j = 0; j < batch_command[3]; j++) { // find texture couple in face list (loop through vertices)
-							if      (k == 0 && faces[absolute].pair1[0] == j) { index = faces[absolute].pair1[1]; break; }
-							else if (k == 1 && faces[absolute].pair2[0] == j) { index = faces[absolute].pair2[1]; break; }
-							else if (k == 2 && faces[absolute].pair3[0] == j) { index = faces[absolute].pair3[1]; break; }
-							
-						}
-						
-						object->texture_coords[old_vertex_count + j].x = (GLfloat) texture_coords[index].x / FLOAT_ONE;
-						object->texture_coords[old_vertex_count + j].y = (GLfloat) texture_coords[index].y / FLOAT_ONE;
-						
-					}
-					
-				}
-				
-				object->indices[i * 3    ] = (uint32_t) faces[absolute].pair1[0];
-				object->indices[i * 3 + 1] = (uint32_t) faces[absolute].pair2[0];
-				object->indices[i * 3 + 2] = (uint32_t) faces[absolute].pair3[0];
+		} else if (batch_command[0] == 'u') { // colour
+			for (unsigned long long i = 0; i < object->vertex_count; i++) {
+				object->colours[i].r = (float) ((unsigned long long) batch_command[2]) / _UI64_MAX;
+				object->colours[i].g = (float) ((unsigned long long) batch_command[3]) / _UI64_MAX;
+				object->colours[i].b = (float) ((unsigned long long) batch_command[4]) / _UI64_MAX;
+				object->colours[i].a = (float) ((unsigned long long) batch_command[5]) / _UI64_MAX;
 				
 			}
 			
