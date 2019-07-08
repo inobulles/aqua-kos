7d0fea1dc4e651d595f54b6b9ea45cff8e5de430
Obiwac <obiwac@gmail.com>
2019-07-08 16:12:44 +0200

    Fixed merge conflicts

--- src/devices/sound.h
+++ src/devices/sound.h
@@ -8,17 +8,10 @@
 	#include <sys/mman.h>
 	
 	#if !KOS_USES_JNI
-<<<<<<< HEAD
-		#include <mad.h>
+		#include <mad.h> // for mp3.h
 		
 		#include <pulse/simple.h>
 		#include <pulse/error.h>
-=======
-		#include <mad.h> // for mp3.h
-		
-		#include <pulse/pulseaudio.h>
-		#include <pulse/volume.h>
->>>>>>> 8b974f9598e7e013084c640eb2b679daea74604d
 	#endif
 	
 	typedef struct {
@@ -53,17 +46,15 @@
 			struct mad_stream stream;
 			struct mad_synth  synth;
 			struct mad_frame  frame;
+			
+			pthread_t thread;
 		#endif
 		
 	} sound_t;
 	
 	#if !KOS_USES_JNI
-		static sound_t* pulse_current_sound;
-		
-		static pa_threaded_mainloop* pulse_mainloop;
-		static pa_mainloop_api*      pulse_api;
-		static pa_context*           pulse_context;
-		static pa_stream*            pulse_stream;
+		static pa_simple* pulse_device = (pa_simple*) 0;
+		static int        pulse_error  = 0;
 		
 		static inline int sound_scale(mad_fixed_t sample) {
 			sample += (1L << (MAD_F_FRACBITS - 16));
@@ -73,7 +64,6 @@
 			
 			return sample >> (MAD_F_FRACBITS + 1 - 16);
 			
-<<<<<<< HEAD
 		} static void sound_output(sound_t* self, struct mad_header const* header, struct mad_pcm* pcm) {
 			const mad_fixed_t* left  = pcm->samples[0];
 			const mad_fixed_t* right = pcm->samples[1];
@@ -96,61 +86,38 @@
 				
 			} if (pa_simple_write(pulse_device, stream, (size_t) BYTES, &pulse_error)) {
 				printf("WARNING PulseAudio failed to write (%s)\n", pa_strerror(pulse_error));
-=======
-		}
-		
-		void pulse_free(void) {
-			if (pulse_mainloop) {
-				/// TODO
->>>>>>> 8b974f9598e7e013084c640eb2b679daea74604d
 				
 			}
 			
 		}
 		
-		void pulse_context_state_callback(pa_context* context, void* mainloop) { pa_threaded_mainloop_signal(mainloop, 0); }
-		void pulse_state_callback        (pa_stream*  stream,  void* mainloop) { pa_threaded_mainloop_signal(mainloop, 0); }
-		
-		void pulse_write_callback(pa_stream* stream, size_t requested_bytes, void* data) {
-			sound_t* self = pulse_current_sound;
-			if (!self) return;
+		static void* sound_loop(void* __self) {
+			sound_t* self = (sound_t*) __self;
 			
-			int remaining = requested_bytes;
-			while (remaining > 0) {
-				uint16_t* buffer = NULL;
-				
-				if (mad_frame_decode(&self->frame, &self->stream)) {
+			while (self->playing) {
+				if (self->paused) {
+					usleep(20);
+					continue;
+					
+				} if (mad_frame_decode(&self->frame, &self->stream)) {
 					if (MAD_RECOVERABLE(self->stream.error) || self->stream.error == MAD_ERROR_BUFLEN) continue;
 					else break;
 					
 				}
 				
 				mad_synth_frame(&self->synth, &self->frame);
-				// &self->frame.header, &self->synth.pcm
-				
-				size_t chunk_bytes = 1152 * 4;
-				if (chunk_bytes > remaining) chunk_bytes = remaining;
-				
-				pa_stream_begin_write(stream, (void**) &buffer, &chunk_bytes);
-				
-				mad_fixed_t const* left  = self->synth.pcm.samples[0];
-				mad_fixed_t const* right = self->synth.pcm.samples[1];
-				
-				for (int i = 0; i < self->synth.pcm.length; i++) {
-					buffer[(self->synth.pcm.length - i) * 2    ] = self->values[0] = sound_scale(* left++);
-					buffer[(self->synth.pcm.length - i) * 2 + 1] = self->values[1] = sound_scale(*right++);
-					
-				}
-				
-				pa_stream_write(pulse_stream, buffer, chunk_bytes, NULL, 0LL, PA_SEEK_RELATIVE);
-				remaining -= chunk_bytes;
+				sound_output(self, &self->frame.header, &self->synth.pcm);
 				
 			}
 			
-		}
-		
-		void pulse_success_callback(pa_stream* stream, int success, void* data) {
-			return;
+			self->playing = 0;
+			pthread_exit(NULL);
+			
+		} void pulse_free(void) {
+			if (pulse_device) {
+				pa_simple_free(pulse_device);
+				
+			}
 			
 		}
 	#endif
@@ -160,74 +127,13 @@
 			return 1; /// TODO
 		#else
 			pulse_free();
-			if (!(pulse_mainloop = pa_threaded_mainloop_new())) {
-				printf("WARNING Failed to create PulseAudio threaded mainloop\n");
-				return 1;
-				
-			} if (!(pulse_context = pa_context_new(pulse_api = pa_threaded_mainloop_get_api(pulse_mainloop), "playback"))) {
-				printf("WARNING Failed to create PulseAudio context\n");
-				return 1;
-				
-			}
-			
-			pa_context_set_state_callback(pulse_context, &pulse_context_state_callback, pulse_mainloop);
-			pa_threaded_mainloop_lock(pulse_mainloop);
-			
-			if (pa_threaded_mainloop_start(pulse_mainloop)) {
-				printf("WARNING Failed to start PulseAudio threaded mainloop\n");
-				return 1;
-				
-			} if (pa_context_connect(pulse_context, NULL, PA_CONTEXT_NOAUTOSPAWN, NULL)) {
-				printf("WARNING Failed to connect PulseAudio context\n");
-				return 1;
-				
-			} while (1) {
-				pa_context_state_t state = pa_context_get_state(pulse_context);
-				if (!PA_CONTEXT_IS_GOOD(state)) {
-					printf("WARNING PulseAudio context is not good\n");
-					return 1;
-					
-				} if (state == PA_CONTEXT_READY) {
-					break;
-					
-				}
-				
-				pa_threaded_mainloop_wait(pulse_mainloop);
-				
-			}
 			
 			pa_sample_spec spec = { .format = format, .rate = frequency, .channels = channels };
-			pa_channel_map map;
-			pa_channel_map_init_stereo(&map);
-			
-			pulse_stream = pa_stream_new(pulse_context, "AQUA sound connection", &spec, &map);
-			
-			pa_stream_set_state_callback(pulse_stream, pulse_state_callback, pulse_mainloop);
-			pa_stream_set_write_callback(pulse_stream, pulse_write_callback, pulse_mainloop);
-			
-			pa_buffer_attr attr = { .maxlength = (uint32_t) -1, .tlength = (uint32_t) -1, .prebuf = (uint32_t) -1, .minreq = (uint32_t) -1 };
-			pa_stream_flags_t flags = PA_STREAM_START_CORKED | PA_STREAM_INTERPOLATE_TIMING | PA_STREAM_NOT_MONOTONIC | PA_STREAM_AUTO_TIMING_UPDATE | PA_STREAM_ADJUST_LATENCY;
-			
-			if (pa_stream_connect_playback(pulse_stream, NULL, &attr, flags, NULL, NULL)) {
-				printf("WARNING Failed to connect PulseAudio stream to playback\n");
+			if (!(pulse_device = pa_simple_new(NULL, "AQUA sound connection", PA_STREAM_PLAYBACK, NULL, "playback", &spec, NULL, NULL, &pulse_error))) {
+				printf("WARNING Failed to create a PulseAudio connection (%s)\n", pa_strerror(pulse_error));
 				return 1;
 				
-			} while (1) {
-				pa_stream_state_t state = pa_stream_get_state(pulse_stream);
-				if (!PA_STREAM_IS_GOOD(state)) {
-					printf("WARNING PulseAudio stream is not good\n");
-					return 1;
-					
-				} if (state == PA_STREAM_READY) {
-					break;
-					
-				}
-				
-				pa_threaded_mainloop_wait(pulse_mainloop);
-				
 			}
-			
-			pa_threaded_mainloop_unlock(pulse_mainloop);
 		#endif
 		
 		return 0;
@@ -240,6 +146,12 @@
 				return 1; /// TODO
 			#else
 				self->playing = 0;
+				
+				if (self->thread) {
+					pthread_join(self->thread, NULL);
+					self->thread = (pthread_t) NULL;
+					
+				}
 			#endif
 			
 		} else if (action == 1) { // pause
@@ -285,8 +197,12 @@
 		#if KOS_USES_JNI
 			return 1; /// TODO
 		#else
-			pulse_current_sound = self;
-			pa_stream_cork(pulse_stream, 0, pulse_success_callback, pulse_mainloop);
+			int temp_error = pthread_create(&self->thread, NULL, sound_loop, (void*) self);
+			if (temp_error) {
+				printf("WARNING Failed to play sound (%d)\n", temp_error);
+				return 1;
+				
+			}
 		#endif
 		
 		return 0;
