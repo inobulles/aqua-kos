62870118c2a8e46350f20fc1a24d0e2f76803c80
Obiwac <obiwac@gmail.com>
2019-07-13 17:31:52 +0200

    Small refresh

--- src/devices/platform.h
+++ src/devices/platform.h
@@ -8,6 +8,7 @@
 	
 	static void platform_device_handle(unsigned long long** result, const char* data) {
 		memset(kos_bda_implementation.temp_string, 0, sizeof(kos_bda_implementation.temp_string));
+		*result = (unsigned long long*) kos_bda_implementation.temp_string;
 		
 		if (strcmp(data, "nickname") == 0) {
 			#if KOS_USES_JNI
@@ -22,8 +23,6 @@
 				else          strcpy (kos_bda_implementation.temp_string, "username");
 			#endif
 			
-			*result = (unsigned long long*) kos_bda_implementation.temp_string;
-			
 		} else if (strcmp(data, "model") == 0) {
 			#if KOS_USES_JNI
 				jstring string = (jstring) CALLBACK(java_platform, callback_env->CallStaticObjectMethod, callback_env->NewStringUTF("model"));
@@ -36,11 +35,8 @@
 				}
 			#endif
 			
-			*result = (unsigned long long*) kos_bda_implementation.temp_string;
-			
 		} else if (strcmp(data, "cpu") == 0) {
 			strcpy(kos_bda_implementation.temp_string, "cpu"); /// TODO
-			*result = (unsigned long long*) kos_bda_implementation.temp_string;
 			
 		} else if (strcmp(data, "vendor") == 0) {
 			#if KOS_USES_JNI
@@ -51,7 +47,40 @@
 				strcpy(kos_bda_implementation.temp_string, "vendor"); /// TODO
 			#endif
 			
-			*result = (unsigned long long*) kos_bda_implementation.temp_string;
+		} else if (strcmp(data, "vm vstring") == 0) {
+			unsigned long long zvm_version     (void);
+			unsigned long long zvm_stack_bytes (void);
+			const char*        zvm_acceleration(void);
+			
+			sprintf(kos_bda_implementation.temp_string, "ZVM (Zed Virtual Machine) v%lld (0x%llxb stack, %s acceleration)", zvm_version(), zvm_stack_bytes(), zvm_acceleration());
+			
+		} else if (strcmp(data, "kos info") == 0) {
+			#define __STRINGIZE(x) #x // from root.h from lib
+			#define   STRINGIZE(x) __STRINGIZE(x)
+			
+			#if KOS_USES_OPENGL
+				#define KOS_INFO_GLIB_STRING "OpenGL " STRINGIZE(KOS_HIGHEST_GL_VERSION_MAJOR) "." STRINGIZE(KOS_HIGHEST_GL_VERSION_MINOR)
+				
+				#if KOS_USES_OPENGL_DESKTOP
+					#define KOS_INFO_GLIB_MORE_STRING "desktop"
+				#elif KOS_USES_OPENGLES
+					#define KOS_INFO_GLIB_MORE_STRING "ES"
+				#endif
+			#else
+				#define KOS_INFO_GLIB_STRING "no GLIB"
+			#endif
+			
+			#if KOS_USES_JNI
+				#define KOS_INFO_INTERFACE "JNI/NDK"
+			#elif KOS_USES_SDL2
+				#define KOS_INFO_INTERFACE "SDL2"
+			#elif KOS_USES_BCM
+				#define KOS_INFO_INTERFACE "BCM"
+			#else
+				#define KOS_INFO_INTERFACE "plain"
+			#endif
+			
+			strcpy(kos_bda_implementation.temp_string, "\"Mega\" (universal) KOS using " KOS_INFO_GLIB_STRING " (" KOS_INFO_GLIB_MORE_STRING ") with the " KOS_INFO_INTERFACE " interface");
 			
 		} else {
 			KOS_DEVICE_COMMAND_WARNING("platform")
