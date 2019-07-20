f2909c5fe926fc7cb3e99e5f231b508346957bb0
Obiwac <obiwac@gmail.com>
2019-07-20 23:17:37 +0200

    Hack to get KOS running without Discord RPC (FIXME)

--- src/devices/discord.h
+++ src/devices/discord.h
@@ -132,10 +132,10 @@
 	static void discord_device_handle(unsigned long long** result, const char* data) {
 		discord_device_struct_t* discord_device_struct = (discord_device_struct_t*) data + 9;
 		
-		if      (strcmp(extra, "rpc init") == 0) init_discord_rpc  ((unsigned long long) data);
-		else if (strcmp(extra, "rpc loop") == 0) loop_discord_rpc  (&discord_device_struct->discord_rpc);
-		else if (strcmp(extra, "rpc updt") == 0) update_discord_rpc(&discord_device_struct->discord_rpc);
-		else if (strcmp(extra, "rpc disp") == 0) dispose_discord_rpc();
+		if      (strcmp(data, "rpc init") == 0) init_discord_rpc  ((unsigned long long) data);
+		else if (strcmp(data, "rpc loop") == 0) loop_discord_rpc  (&discord_device_struct->discord_rpc);
+		else if (strcmp(data, "rpc updt") == 0) update_discord_rpc(&discord_device_struct->discord_rpc);
+		else if (strcmp(data, "rpc disp") == 0) dispose_discord_rpc();
 		else KOS_DEVICE_COMMAND_WARNING("discord")
 		
 	}
