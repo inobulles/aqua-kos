992b6708b2d37bb35bd3682b43695b1f2dff5349
Obiwac <obiwac@gmail.com>
2020-04-11 23:19:17 +0200

    Started on big restructuring of AQUA

--- glue.c
+++ glue.c
@@ -5,9 +5,11 @@
 #include <assert.h>
 #include <signal.h>
 #include <stdint.h>
+#include <string.h>
 #include <dlfcn.h>
 #include <dirent.h>
 
+#include "src/iar.h"
 #include "src/kos.h"
 
 // functions left to implement
@@ -24,68 +26,113 @@ void kos_current_machine    (void) { printf("IMPLEMENT %s\n", __func__); }
 
 #include "zvm/zvm.h"
 
-#define ROM_PATH "rom.zed"
-static zvm_program_t* de_program;
+#define ROOT_PATH "root"
+#define BOOT_PATH "root/boot.zpk"
 
-static int load_rom(const char* path, char** rom, unsigned long long* bytes) {
-	FILE* fp = fopen(path, "rb");
-	
-	if (!fp) {
-		printf("WARNING Could not open ROM file (%s)\n", path);
-		return 1;
-	
-	}
-	
-	fseek(fp, 0, SEEK_END);
-	*bytes = (unsigned long long) ftell(fp);
-	rewind(fp);
-	
-	*rom = (char*) malloc(*bytes);
-	fread(*rom, sizeof(char), *bytes, fp);
-	fclose(fp);
-	
-	return 0;
-}
+static zvm_program_t* de_program;
+static iar_file_t boot_package;
 
 int main(int argc, char** argv) {
+	char* root_path = (char*) ROOT_PATH;
+	char* boot_path = (char*) BOOT_PATH;
+	
 	printf("Parsing arguments ...\n");
-	char* path;
+	for (int i = 1; i < argc; i++) {
+		if (strncmp(argv[i], "--", 2) == 0) { // argument is option
+			char* option = argv[i] + 2;
+			
+			if (strcmp(option, "help") == 0) {
+				printf("AQUA KOS command-line executable help\n");
+				printf("`--help`: Print out help.\n");
+				printf("`--root [root directory path]`: Specify where the root directory is.\n");
+				printf("`--boot [boot package path]`: Specifiy where the boot package is. Note that this is relative to the working directory, not the root directory.\n");
+				return 0;
+				
+			}
+			
+			else if (strcmp(option, "root") == 0) root_path = argv[++i];
+			else if (strcmp(option, "boot") == 0) boot_path = argv[++i];
+			
+			else {
+				fprintf(stderr, "ERROR Option `--%s` is unknown. Run `aqua --help` to see a list of available options\n", option);
+				return 1;
+			}
+		} else {
+			fprintf(stderr, "ERROR Unexpected argument `%s`\n", argv[i]);
+			return 1;
+		}
+	}
 	
-	if (argc <= 1) path = (char*) ROM_PATH;
-	else           path = argv[1];
+	printf("Reading the boot package (%s) ...\n", boot_path);
+	if (iar_open(&boot_package, boot_path)) return 1;
 	
-	printf("Loading the KOS ...\n");
-	load_kos();
+	printf("Finding start node ...\n");
 	
-	printf("Loading the DE ...\n");
+	iar_node_t start_node;
+	if (iar_find_node(&boot_package, &start_node, "start", &boot_package.root_node) == -1) {
+		fprintf(stderr, "ERROR Failed to find start node in boot package\n");
+		//iar_free(&boot_package); // don't care about freeing this; no risk for memory leaks
+		return 1;
+	}
 	
-	char* rom = (char*) 0;
-	unsigned long long bytes = 0;
+	printf("Reading start node ...\n");
+	if (!start_node.data_bytes) {
+		fprintf(stderr, "ERROR Start node empty\n");
+		return 1;
+	}
 	
-	int error_code = load_rom(path, &rom, &bytes);
-	if (error_code) {
-		printf("ERROR Failed to load ROM (error code = %d), aborting ...\n", error_code);
+	char* start_command = (char*) malloc(start_node.data_bytes);
+	if (iar_read_node_contents(&boot_package, &start_node, start_command)) {
+		return 1;
+	}
+	
+	if (strncmp(start_command, "zed", 3) == 0) {
+		printf("Start command is zed, finding ROM node ...\n");
+		
+		iar_node_t rom_node;
+		if (iar_find_node(&boot_package, &rom_node, "rom.zed", &boot_package.root_node) == -1) {
+			fprintf(stderr, "ERROR Failed to find ROM node (rom.zed) in boot package\n");
+			return 1;
+		}
+		
+		printf("Reading rom node ...\n");
+		if (!rom_node.data_bytes) {
+			fprintf(stderr, "ERROR ROM node empty\n");
+			return 1;
+		}
+		
+		char* rom = (char*) malloc(rom_node.data_bytes);
+		if (iar_read_node_contents(&boot_package, &rom_node, rom)) {
+			return 1;
+		}
+		
+		printf("Loading the KOS ...\n");
+		load_kos();
+		
+		printf("Loading the DE ...\n");
+		
+		de_program = (zvm_program_t*) malloc(sizeof(zvm_program_t));
+		memset(de_program, 0, sizeof(zvm_program_t));
+		de_program->pointer = rom;
+		
+		printf("Starting run setup phase ...\n");
+		zvm_program_run_setup_phase(de_program);
+		
+		while (!zvm_program_run_loop_phase(de_program));
+		int error_code = de_program->error_code;
+		
+		zvm_program_free(de_program);
+		free(rom);
+		
+		printf("DE return code is %d\n", error_code);
+		
+		printf("Quitting KOS ...\n");
+		quit_kos();
+		free(de_program);
+		
 		return error_code;
 	}
 	
-	de_program = (zvm_program_t*) malloc(sizeof(zvm_program_t));
-	memset(de_program, 0, sizeof(zvm_program_t));
-	de_program->pointer = rom;
-	
-	printf("Starting run setup phase ...\n");
-	zvm_program_run_setup_phase(de_program);
-	
-	while (!zvm_program_run_loop_phase(de_program));
-	error_code = de_program->error_code;
-	
-	zvm_program_free(de_program);
-	free(rom);
-	
-	printf("DE return code is %d\n", error_code);
-	
-	printf("Quitting KOS ...\n");
-	quit_kos();
-	free(de_program);
-	
-	return error_code;
+	printf("ERROR Unknown start command %s\n", start_command);
+	return 1;
 }
---
+++ src/iar.h
@@ -0,0 +1,88 @@
+#define IAR_MAGIC 0x1A4C1A4C1A4C1A4C
+
+typedef struct {
+	uint64_t magic;
+	uint64_t version;
+	uint64_t root_node_offset;
+} iar_header_t;
+
+typedef struct {
+	uint64_t node_count;
+	uint64_t node_offsets_offset;
+	uint64_t name_offset;
+	uint64_t data_bytes;
+	uint64_t data_offset;
+} iar_node_t;
+
+typedef struct {
+	FILE* fp;
+	int fd;
+	
+	iar_header_t header;
+	iar_node_t root_node;
+} iar_file_t;
+
+int iar_open(iar_file_t* self, const char* path) {
+	self->fp = fopen(path, "r");
+	if (!self->fp) {
+		printf("ERROR Failed to read %s\n", path);
+		return 1;
+	}
+	
+	self->fd = fileno(self->fp);
+	pread(self->fd, &self->header, sizeof(self->header), 0);
+	
+	if (self->header.magic != IAR_MAGIC) {
+		printf("ERROR %s is not a valid IAR file\n", path);
+		fclose(self->fp);
+		return 1;
+	}
+	
+	if (self->header.version != 0) {
+		printf("ERROR %s is of an invalid version (%lu) (AQUA only supports IAR version 0)\n", path, self->header.version);
+		fclose(self->fp);
+		return 1;
+	}
+	
+	pread(self->fd, &self->root_node, sizeof(self->root_node), self->header.root_node_offset);
+	return 0;
+}
+
+uint64_t iar_find_node(iar_file_t* self, iar_node_t* node, const char* name, iar_node_t* parent) { // return file index of found file or -1 if nothing found, 
+	uint64_t node_offsets_bytes = parent->node_count * sizeof(uint64_t);
+	uint64_t* node_offsets = (uint64_t*) malloc(node_offsets_bytes);
+	pread(self->fd, node_offsets, node_offsets_bytes, parent->node_offsets_offset);
+	
+	uint64_t found = -1;
+	for (int i = 0; i < parent->node_count; i++) {
+		iar_node_t child_node;
+		pread(self->fd, &child_node, sizeof(child_node), node_offsets[i]);
+		
+		char node_name[256]; // don't think many names will be larger than 256 tbh
+		pread(self->fd, node_name, sizeof(node_name), child_node.name_offset); // don't have to worry about node->name_offset + sizeof(node_name) potentially being bigger than file, since pread will stop reading if it reaches eof
+		
+		if (strncmp(name, node_name, sizeof(node_name)) == 0) {
+			memcpy(node, &child_node, sizeof(child_node));
+			found = i;
+			break;
+		}
+	}
+	
+	free(node_offsets);
+	return found;
+}
+
+int iar_read_node_contents(iar_file_t* self, iar_node_t* node, char* buffer) {
+	if (node->data_offset) {
+		pread(self->fd, buffer, node->data_bytes, node->data_offset);
+		return 0;
+		
+	} else {
+		printf("ERROR Provided node is not a file and thus contains no data\n");
+		return 1;
+	}
+}
+
+void iar_free(iar_file_t* self) {
+	fclose(self->fp);
+}
